aec6b03c17029ca0e347bf32e392b012f1cf595f
gabmellofull <gmellocorp@gmail.com>
2021-10-29 23:50:10 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,6 +33,10 @@ public class Program {
             System.out.println(sell);
         }
 
+        System.out.println("\n=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
 
     }
 
